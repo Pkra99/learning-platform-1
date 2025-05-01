ff07597eb7f07314498dc698e2df0e42d202dc34
Pkra99 <58003435+Pkra99@users.noreply.github.com>
2025-05-01 19:23:20 +0000

    doing changes in contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -88,53 +88,57 @@ export default function Contact() {
               </div>
             </div>
 
-            <form className="p-6 flex flex-col justify-center">
-              <div className="flex flex-col">
-                <label for="name" className="hidden">
-                  Full Name
-                </label>
-                <input
-                  type="name"
-                  name="name"
-                  id="name"
-                  placeholder="Full Name"
-                  className="w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-orange-500 focus:outline-none"
-                />
-              </div>
+            <form 
+  action="https://formsubmit.co/pkpr99bux@gmail.com" 
+  method="POST" 
+  className="p-6 flex flex-col justify-center"
+>
+  {/* Optional: Disable CAPTCHA */}
+  <input type="hidden" name="_captcha" value="false" />
 
-              <div className="flex flex-col mt-2">
-                <label for="email" className="hidden">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  placeholder="Email"
-                  className="w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-orange-500 focus:outline-none"
-                />
-              </div>
+  <div className="flex flex-col">
+    <label htmlFor="name" className="hidden">Full Name</label>
+    <input
+      type="text"
+      name="name"
+      id="name"
+      placeholder="Full Name"
+      required
+      className="w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-orange-500 focus:outline-none"
+    />
+  </div>
 
-              <div className="flex flex-col mt-2">
-                <label for="tel" className="hidden">
-                  Number
-                </label>
-                <input
-                  type="tel"
-                  name="tel"
-                  id="tel"
-                  placeholder="Telephone Number"
-                  className="w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-orange-500 focus:outline-none"
-                />
-              </div>
+  <div className="flex flex-col mt-2">
+    <label htmlFor="email" className="hidden">Email</label>
+    <input
+      type="email"
+      name="email"
+      id="email"
+      placeholder="Email"
+      required
+      className="w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-orange-500 focus:outline-none"
+    />
+  </div>
 
-              <button
-                type="submit"
-                className="md:w-32 bg-orange-700 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-orange-600 transition ease-in-out duration-300"
-              >
-                Submit
-              </button>
-            </form>
+  <div className="flex flex-col mt-2">
+    <label htmlFor="tel" className="hidden">Number</label>
+    <input
+      type="tel"
+      name="tel"
+      id="tel"
+      placeholder="Telephone Number"
+      className="w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-orange-500 focus:outline-none"
+    />
+  </div>
+
+  <button
+    type="submit"
+    className="md:w-32 bg-orange-700 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-orange-600 transition ease-in-out duration-300"
+  >
+    Submit
+  </button>
+</form>
+
           </div>
         </div>
       </div>
